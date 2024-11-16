6bc4cdc315b00e25694b4e51934e43efa46d8dfb
Lucky Ivanius <luckzivanius@gmail.com>
2024-11-17 06:16:51 +0700

    chore: fix contract script and environment

--- deploy-hyperlane.sh
+++ deploy-hyperlane.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # deploy on anvil1 & anvil2
-hyperlane registry init --overrides ./hyperlane \
-&& hyperlane core deploy -r ./hyperlane --key ${PRIVATE_KEY} --chain anvil1 -y \
-& hyperlane core deploy -r ./hyperlane --key ${PRIVATE_KEY} --chain anvil2 -y
+hyperlane core deploy -r ./hyperlane --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --chain anvil1 -y \
+& hyperlane core deploy -r ./hyperlane --key 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 --chain anvil2 -y
